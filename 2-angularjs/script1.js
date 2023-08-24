8f163996df400cea2a5d88f382c9f7eaaaaec604
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-24 15:25:43 +0530

    Updated

--- 2-angularjs/script1.js
+++ 2-angularjs/script1.js
@@ -1,11 +1,22 @@
 ///<reference path="angular.min.js" />
 
-var app=angular.module("module1",[]).controller("controller1",function($scope){
-    person={
-        name:"",
-        gender:"" ,
-        age:""
+var app = angular.module("module1", []).controller("controller1", function ($scope) {
+    person = {
+        name: "",
+        gender: "",
+        age: ""
 
     }
-    $scope.person=person;
+    $scope.person = person;
+})
+
+app.controller("controller2", function ($scope) {
+    var emp = [
+        { firstName: "Pavan", lastName: "Kalyan", role: "Consultant", age: 24 },
+        { firstName: "Abhishek", lastName: "MR", role: "Traine", age: 23 },
+        { firstName: "Prakash", lastName: "Singh", role: "Traine", age: 24 },
+        { firstName: "Prasanjith", lastName: "Kund", role: "Traine", age: 29 },
+        { firstName: "Ravi", lastName: "Singh", role: "MS Trainee", age: 24 },
+    ];
+    $scope.emps = emp;
 })
