6b785bc4951a1f1b390dfbc1c86e2b0e5754bf28
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-25 12:42:06 +0530

    Updated

--- 2-angularjs/script1.js
+++ 2-angularjs/script1.js
@@ -39,18 +39,35 @@ var app=angular.module("module1",[]).controller("controller1",function($scope){
     ]
     $scope.countries=countries;     
     
+    // let instagram=[
+    //     {post:"one",likes:0,dislikes:0},
+    //     {post:"two",likes:0,dislikes:0},
+    //     {post:"tree",likes:0,dislikes:0},
+    //     {post:"four",likes:0,dislikes:0}
+    // ]
+    // $scope.instagram=instagram;
+    // $scope.incLike = function(insta){
+    //     insta.likes++;
+    // }
+
+    // $scope.incDisLike = function(insta){
+    //     insta.dislikes++;
+    // }
+
     let instagram=[
-        {post:"one",likes:0,dislikes:0},
-        {post:"two",likes:0,dislikes:0},
-        {post:"tree",likes:0,dislikes:0},
-        {post:"four",likes:0,dislikes:0}
-    ]
+        {post:"One",like:0,dislike:0},
+        {post:"Two",like:0,dislike:0},
+        {post:"Three",like:0,dislike:0},
+        {post:"Four",like:0,dislike:0},
+        {post:"Five",like:0,dislike:0},
+    ];
     $scope.instagram=instagram;
+
     $scope.incLike = function(insta){
-        insta.likes++;
+        insta.like++;
     }
 
     $scope.incDisLike = function(insta){
-        insta.dislikes++;
+       insta.dislike++;
     }
 })
