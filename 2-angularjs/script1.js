5d16067510525b26f45223ca18e85fa58412ca1e
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-25 11:43:28 +0530

    Updated

--- 2-angularjs/script1.js
+++ 2-angularjs/script1.js
@@ -9,6 +9,33 @@ var app=angular.module("module1",[]).controller("controller1",function($scope){
         {name:"Prakash",role:"Trainee",salary:25000},
         {name:"John",role:"Consultant",salary:75000}
     ]
-
     $scope.emps=emps;
+
+    let countries=[
+        {
+            name:"India",
+            cities:[
+                {name:"Bangalore"},
+                {name:"Chennai"},
+                {name:"Locknow"},
+            ]
+        },
+        {
+            name:"India",
+            cities:[
+                {name:"Bangalore"},
+                {name:"Chennai"},
+                {name:"Locknow"},
+            ]
+        },
+        {
+            name:"India",
+            cities:[
+                {name:"Bangalore"},
+                {name:"Chennai"},
+                {name:"Locknow"},
+            ]
+        }
+    ]
+    $scope.countries=countries;
 })
