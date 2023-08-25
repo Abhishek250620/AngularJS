a80d10803fbeeb66a2011d700d15acae412d8882
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-25 12:23:49 +0530

    Updated

--- 2-angularjs/script1.js
+++ 2-angularjs/script1.js
@@ -37,5 +37,20 @@ var app=angular.module("module1",[]).controller("controller1",function($scope){
             ]
         }
     ]
-    $scope.countries=countries;
+    $scope.countries=countries;     
+    
+    let instagram=[
+        {post:"one",likes:0,dislikes:0},
+        {post:"two",likes:0,dislikes:0},
+        {post:"tree",likes:0,dislikes:0},
+        {post:"four",likes:0,dislikes:0}
+    ]
+    $scope.instagram=instagram;
+    $scope.incLike = function(insta){
+        insta.likes++;
+    }
+
+    $scope.incDisLike = function(insta){
+        insta.dislikes++;
+    }
 })
