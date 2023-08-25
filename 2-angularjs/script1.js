dd523191aff4998023a67953ff2f499431874b74
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-25 11:31:28 +0530

    Updated

--- 2-angularjs/script1.js
+++ 2-angularjs/script1.js
@@ -3,4 +3,12 @@
 var app=angular.module("module1",[]).controller("controller1",function($scope){
     let name="";
     $scope.name=name;
+
+    let emps=[
+        {name:"Pavan",role:"Consultant",salary:50000},
+        {name:"Prakash",role:"Trainee",salary:25000},
+        {name:"John",role:"Consultant",salary:75000}
+    ]
+
+    $scope.emps=emps;
 })
