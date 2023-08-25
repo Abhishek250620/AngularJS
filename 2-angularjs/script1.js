d6a3f343d1cb01d71f22dbb3a008597917c10289
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-25 11:18:29 +0530

    Updated

--- 2-angularjs/script1.js
+++ 2-angularjs/script1.js
@@ -1,46 +1,6 @@
 ///<reference path="angular.min.js" />
 
-var app = angular.module("module1", []).controller("controller1", function ($scope) {
-    person = {
-        name: "",
-        gender: "",
-        age: ""
-
-    }
-    $scope.person = person;
-})
-
-app.controller("controller2", function ($scope) {
-    var emp = [
-        { firstName: "Pavan", lastName: "Kalyan", role: "Consultant", age: 24 },
-        { firstName: "Abhishek", lastName: "MR", role: "Traine", age: 23 },
-        { firstName: "Prakash", lastName: "Singh", role: "Traine", age: 24 },
-        { firstName: "Prasanjith", lastName: "Kund", role: "Traine", age: 29 },
-        { firstName: "Ravi", lastName: "Singh", role: "MS Trainee", age: 24 },
-    ];
-    $scope.emps = emp;
-})
-
-app.controller("controller3", function ($scope) {
-    var countries = [
-        {
-            name: "India",
-            cities: [
-                { name: "Bangalore" }, { name: "chennai" }
-            ]
-        },
-        {
-            name: "US",
-            cities: [
-                { name: "New York" }, { name: "Chikago" },{name:"LA"}
-            ]
-        },
-        {
-            name: "UK",
-            cities: [
-                { name: "lknk" }, { name: "dfvfv" }
-            ]
-        }
-    ];
-    $scope.country = countries;
+var app=angular.module("module1",[]).controller("controller1",function($scope){
+    let name="";
+    $scope.name=name;
 })
