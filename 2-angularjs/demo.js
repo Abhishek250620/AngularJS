fbe9f96c909afd1f1ce12be8ef27b0e30c124942
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-31 14:45:03 +0530

    Updated

---
+++ 2-angularjs/demo.js
@@ -0,0 +1,11 @@
+var a = "  ata"
+
+console.log(a);
+console.log(a.trim());
+
+var str='abhi'
+console.log(str[3]);
+console.log(str.length);
+console.log(typeof(str));
+arr=[1,2,3,4,5]
+console.log(arr);
