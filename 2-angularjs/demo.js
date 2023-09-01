dddd8a89f71ec122fc435b7d0ff7360544e597c8
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-09-01 11:54:09 +0530

    Updated

--- 2-angularjs/demo.js
+++ 2-angularjs/demo.js
@@ -9,3 +9,37 @@ console.log(str.length);
 console.log(typeof(str));
 arr=[1,2,3,4,5]
 console.log(arr);
+
+let demo = () => console.log("Hello world");
+
+demo();
+
+arr.forEach(element => {
+    console.log(element);
+});
+
+let dummy=d =>{
+    console.log("dummy123");
+}
+ let date = new Date();
+ console.log(date);
+ console.log(date.getDate());
+ console.log(date.getDay());
+ console.log(date.getMonth());
+ console.log(date.getFullYear());
+
+  const set = new Set();
+ set.add(123);
+ set.add("wee");
+ set.add('q');
+ set.add(false);
+ set.add(undefined);
+
+ console.log(set);
+ set.forEach(ele=>{
+    console.log(ele);
+ })
+
+ for(ele of set){
+    console.log(ele);
+ }
